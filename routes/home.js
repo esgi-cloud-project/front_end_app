0b6689ef272dba75341a41ac99bba59301fdd148
Joris Demandre <joris.demandre@agence-soon.fr>
2020-02-10 01:30:38 +0100

    use fresh dynamodb data and use api data

--- routes/home.js
+++ routes/home.js
@@ -1,21 +1,45 @@
-var router     = require('express').Router();
+let router     = require('express').Router();
+let http = require('http');
 const AWS      = require('aws-sdk');
-const dynamodb = new AWS.DynamoDB.DocumentClient({region: "eu-west-3", credentials: AWS.config.credentials});
+const dynamodb = new AWS.DynamoDB.DocumentClient({region: "eu-west-3", credentials: credentials});
 
 let products;
-
-dynamodb.scan({
-    TableName: 'Product',
-}, (err, data) => {
-    if (!err) {
-         products = data.Items;
-    } else {
-        console.log(err);
-    }
-});
+let productsFromApi;
+let ApiProductsCallOptions = {
+    host: process.env.BACK_END_URL,
+    port: 80,
+    path: '/api/products',
+    method: 'GET'
+};
 
 router.get('/', function (req, res) {
-    res.render('home', {'products': products});
+    dynamodb.scan({
+        TableName: 'Product',
+    }, (err, data) => {
+        if (!err) {
+            products = data.Items;
+            res.render('home', {'products': products});
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+router.get('/from-api', function (req, res) {
+    http.get(ApiProductsCallOptions, function(response) {
+        let body = '';
+
+        response.on('data', function (chunk) {
+            body+=chunk;
+        });
+
+        response.on('end', function(){
+            productsFromApi = JSON.parse(body).data;
+            res.render('home', {'products': productsFromApi});
+        })
+    }).on('error', function(err){
+        console.log(err);
+    });
 });
 
 module.exports = router;
